c1331c75b91c6f3987c78d16299d8abc5f57f455
riptide16 <abdulhafidh.perpusnas@gmail.com>
2024-06-03 07:41:29 +0700

    Add Onthespot Score in Recap

--- app/Models/Evaluation.php
+++ app/Models/Evaluation.php
@@ -69,6 +69,7 @@ class Evaluation extends Model
             ->get();
 
         foreach ($mainComponents as $comp) {
+
             $contents = \DB::table('evaluation_contents AS ec')
                 ->join('accreditation_contents AS ac', 'ec.accreditation_content_id', 'ac.id')
                 ->join('instrument_aspects AS ia', 'ac.aspectable_id', 'ia.id')
@@ -78,6 +79,17 @@ class Evaluation extends Model
                 ->whereNull('ia.deleted_at')
                 ->select(\DB::raw("COUNT(ec.id) as jml_soal, SUM(ec.value) as total, COUNT(ec.id) * 5 as jml_skor"))
                 ->first();
+
+            $onthespot = \DB::table('evaluation_contents AS ec')
+                ->join('accreditation_contents AS ac', 'ec.accreditation_content_id', 'ac.id')
+                ->join('instrument_aspects AS ia', 'ac.aspectable_id', 'ia.id')
+                ->join('instrument_components AS ic', 'ia.instrument_component_id', 'ic.id')
+                ->where('ec.evaluation_id', $this->id)
+                ->where('ac.main_component_id', $comp->main_component_id)
+                ->whereNull('ia.deleted_at')
+                ->select(\DB::raw("ec.value as total"))
+                ->get();    
+
             $result[] = [
                 'instrument_component' => $comp->name,
                 'instrument_component_id' => $comp->main_component_id,
@@ -86,7 +98,9 @@ class Evaluation extends Model
                 'total_score' => $contents->jml_skor,
                 'weight' => $comp->weight,
                 'score' => $contents->jml_soal ? $this->calculateScore($contents->jml_soal, $contents->total, $comp->weight) : 0,
+                'onthespot' => $onthespot,
             ];
+
         }
 
         $this->evaluationResult = $result;
